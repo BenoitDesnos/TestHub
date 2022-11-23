5b72c1d922dfb8c5884ceea28ee9b876d847ebe8
BenoitDesnos <103570316+BenoitDesnos@users.noreply.github.com>
2022-11-23 15:59:26 +0100

    bug score moyen résolu + supprime jeu home

--- src/components/GameChoice.jsx
+++ src/components/GameChoice.jsx
@@ -2,37 +2,37 @@ import { Link } from "react-router-dom";
 function GameChoice() {
   return (
     <>
-      <h1 className="home__title">Checkout our latest tests !</h1>
+      <h1 className="home__title">Découvrez nos tests !</h1>
       <div className="games">
         <Link to="/reaction" className="games__card">
           <i className="fa-solid fa-bolt-lightning"></i>
           <h3>Reaction time</h3>
-          <p>Test your visual reflexes</p>
+          <p>Testez vos réflèxes visuels</p>
         </Link>
         <Link to="/" className="games__card disabled">
           <i className="fa-solid fa-crosshairs"></i>
           <h3>Aim trainer</h3>
-          <p>How quickly can you hit all the targets?</p>
+          <p>A quelle vitesse pouvez vous détruire les cibles?</p>
         </Link>
         <Link to="/" className="games__card disabled">
           <i className="fa-solid fa-eye"></i>
           <h3>Visual Memory</h3>
-          <p>Remember an increasingly large board of squares.</p>
+          <p>Souvenez-vous des positions des carrés !</p>
         </Link>
         <Link to="/" className="games__card disabled">
           <i className="fa-solid fa-database"></i>
           <h3>Number Memory</h3>
-          <p>Remember the longest number you can.</p>
+          <p>Souvenez-vous du nombre le plus grand possible.</p>
         </Link>
         <Link to="/" className="games__card disabled">
           <i className="fa-solid fa-book"></i>
           <h3>Verbal Memory</h3>
-          <p>Keep as many words in short term memory as possible.</p>
+          <p>Souvenez-vous du plus de mots possible.</p>
         </Link>
         <Link to="/" className="games__card disabled">
           <i className="fa-solid fa-keyboard"></i>
           <h3>Typing</h3>
-          <p>How many words per minute can you type?</p>
+          <p>Combien de mots par minutes peux-tu écrire?</p>
         </Link>
       </div>
     </>
